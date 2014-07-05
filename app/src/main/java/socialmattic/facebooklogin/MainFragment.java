08bd8a5900a8fb5f3c7da2ae521d2772256320e6
Blake Gideon <bgideon@luc.edu>
2014-07-05 15:00:08 -0500

    A bare-bones android application with Facebook Login

---
+++ app/src/main/java/socialmattic/facebooklogin/MainFragment.java
@@ -0,0 +1,22 @@
+package socialmattic.facebooklogin;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by bjg on 7/5/14.
+ */
+public class MainFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_login_, container, false);
+
+        return view;
+    }
+}
